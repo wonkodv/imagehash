b379ce53fac2559f352913d7bc8b2acbc26a29bf
Wonko <wonko@hanstool.org>
2017-06-03 04:48:55 +0200

    WIP: main: show all fingerprints if one file is passed

--- imagehash/__main__.py
+++ imagehash/__main__.py
@@ -3,18 +3,27 @@ from . import imagehash
 import sys
 from PIL import Image
 
-_, method, cutoff, *files = sys.argv
-cutoff = int(cutoff)
 
-method = getattr(imagehash, method)
+if len(sys.argv) > 2:
+    _, method, cutoff, *files = sys.argv
+    cutoff = int(cutoff)
 
-l = []
+    method = getattr(imagehash, method)
 
-for f in files:
-    h = method(Image.open(f))
-    for ho, fo in l:
-        d = ho - h
-        if d <= cutoff:
-            print (d,f,fo)
-    l.append((h,f))
+    l = []
 
+    for f in files:
+        h = method(Image.open(f))
+        for ho, fo in l:
+            d = ho - h
+            if d <= cutoff:
+                print (d,f,fo)
+        l.append((h,f))
+
+else:
+    f = sys.argv[1]
+    i = Image.open(f)
+    for n in 'average_hash','dhash':
+        c = getattr(imagehash, n)
+        if callable(c):
+            print(n, c(i))
